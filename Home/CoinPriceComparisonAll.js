b46982de19af46155bf325d1ffc377477b4321c7
Dongmin Shin <shdomi@naver.com>
2022-12-29 18:13:46 +0900

    +Free_Board_Content/Working_Time_1H

--- Home/CoinPriceComparisonAll.js
+++ Home/CoinPriceComparisonAll.js
@@ -11,6 +11,8 @@ let upbitValueArr = [];
 let allCoin = {비트코인: 'BTC', 리플: 'XRP', 이더리움 : 'ETH',디비전: 'DVI', 쿠사마: 'KSM', 네스트리: 'EGG', 밸런서: 'BAL', 퀴즈톡: 'QTCON', 프론티어: 'FRONT', 고머니: 'GOM2', 제로엑스: 'ZRX', 리니어파이낸스: 'LINA', 저스트: 'JST', 썬: 'SUN', 오미세고: 'OMG', 스와이프: 'SXP', 에이다: 'ADA', 렌: 'REN', 위드: 'WIKEN', 알케미페이: 'ACH', 벨라프로토콜: 'BEL', 어셈블프로토콜: 'ASM', 오르빗체인: 'ORC', 식스: 'SIX', 더그래프: 'GRT', 페이코인: 'PCI', 우마: 'UMA', 카르테시: 'CTSI', 코스모스: 'ATOM', 클레이튼: 'KLAY', 울트라: 'UOS', 유니스왑: 'UNI', 네오핀: 'NPT', 너보스: 'CKB', 힙스: 'HIBS', 맵프로토콜: 'MAP', 아발란체: 'AVAX', 템코: 'TEMCO', 컴파운드: 'COMP', 뱅코르: 'BNT', 다오메이커: 'DAO', 오리진프로토콜: 'OGN', 알파: 'ARPA', 이더리움: 'ETH', 스시스왑: 'SUSHI', 소다코인: 'SOC', 콘텐토스: 'COS', 신세틱스: 'SNX', 갈라: 'GALA', 미버스: 'MEV', 마일벌스: 'MVC', 아모코인: 'AMO', 에이피이앤에프티: 'NFT', 베이직: 'BASIC', 연파이낸스: 'YFI', 바이낸스코인: 'BNB', 다이: 'DAI', 메이커: 'MKR', 루프링: 'LRC', 랠리: 'RLY', 티드랍: 'TDROP', 엘론드: 'EGLD', 라이브피어: 'LPT', 오션프로토콜: 'OCEAN', 엘프: 'ELF', 파워렛저: 'POWR', 스트라티스: 'STRAX', 스테이터스네트워크토큰: 'SNT', 코르텍스: 'CTXC', 웨이브: 'WAVES', 이오스트: 'IOST', 라이즌: 'ATOLO', 밸러토큰: 'VALOR', 코넌: 'CON', 믹스마블: 'MIX', 펑션엑스: 'FX', 크로미아: 'CHR', 머신익스체인지코인: 'MXC', 트러스트버스: 'TRV', 왐토큰: 'WOM', 보아: 'BOA', 엘리시아: 'EL', 프로톤: 'XPR', 베라시티: 'VRA', 피트네트워크: 'FIT', 크레딧코인: 'CTC', 에이피엠코인: 'APM', 센트럴리티: 'CENNZ', 이브이지: 'EVZ', 리저브라이트: 'RSR', 뉴메레르: 'NMR', 아이젝: 'RLC', 옵저버: 'OBSR', 폴라리스쉐어: 'POLA', 에이아이워크: 'AWO', 어댑터토큰: 'ADP', 게이머코인: 'GHX', 블로서리: 'BLY', 이포스: 'WOZX', 애니버스: 'ANV', 바이오패스포트: 'BIOT', 제노토큰: 'XNO', 라이파이낸스: 'SOFI', 콜라토큰: 'COLA', 오키드: 'OXT', 플레이댑: 'PLA', 셀러네트워크: 'CELR', 아로와나토큰: 'ARW', 바이프로스트: 'BFC', 마이네이버앨리스: 'ALICE', 코티: 'COTI', 팬케이크스왑: 'CAKE', 비너스: 'XVS', 트러스트스왑: 'SWAP', 우네트워크: 'WOO', 벨로프로토콜: 'VELO', 웨이키체인: 'WICC', 썸씽: 'SSX', 셴투: 'CTK', 심볼: 'XYM', 고체인: 'GO', 디파인: 'DFA', 코인: 'C98', 보바토큰: 'BOBA', 이피니티토큰: 'EFI', 재스미코인: 'JASMY', 타이탄스왑: 'TITAN', 리퀘스트: 'REQ', 캐스퍼: 'CSPR', 스포티움: 'SPRT', 레이: 'REI', 마브렉스: 'MBX', 알타바: 'TAVA', 아치루트: 'ALT', 체인: 'XCN', 갤럭시아: 'GXA', 아지트: 'AZIT', 스텝앱: 'FITFI'};
 
 
+
+
 let coinoneObj = {};
 
 
@@ -36,6 +38,7 @@ for ( let z = 0; z < upbitValueArr.length; z++) {
   for ( let z = 0; z < upbitValueArr.length; z++) {
     upbitEngObj[upbitEngName[z]] = upbitKeyArr[z]
   }
+  allCoin = Object.assign(allCoin,upbitObj)
 }
 
 
